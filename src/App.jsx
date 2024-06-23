0e8d0c15b651beb11cbe1871f61b6899a99b9d0d
ChuluSimama <137181860+ChuluSimama@users.noreply.github.com>
2024-06-23 23:08:15 +0200

    VanLife

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,54 @@
-/**
- * Challenge:
- * Bootstrap the VanLife project by creating the first 2 routes:
- * Home and About.
- * 
- * Also include the navbar that can link between the two routes.
- * For now, you'll either need to copy/paste the navbar code
- * to both Home and About pages, or you'll need to find a place
- * to put it where it can be shared between the two pages.
- * (Don't overthink this part - just do whatever is easiest for
- * you because we'll learn a better approach very soon)
- * 
- * Review challenge: do all the CSS yourself based on the design
- * linked in the slides.
- */
+import React from "react";
+import {BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home.jsx";
+import About from "./Pages/About.jsx";
+import Vans from "./Pages/Vans/Vans.jsx";
+import VanDetail from "./Pages/Vans/VanDetail.jsx";
+import Layout from "./components/Layout.jsx";
+import HostLayout from "./components/HostLayout.jsx";
+import AuthRequired from "./components/AuthRequired.jsx";
+import Login from "./Pages/Login.jsx";
+import Dashboard from "./Pages/Host/Dashboard.jsx";
+import NotFound from "./Pages/NotFound.jsx";
+import HostVanDetail from "./Pages/Host/HostVanDetail.jsx";
+import Income from "./Pages/Host/Income.jsx";
+import Reviews from "./Pages/Host/Reviews.jsx";
+import HostVans from "./Pages/Host/HostVans.jsx";
+import HostVanInfo from "./Pages/Host/HostVanInfo.jsx";
+import HostVanPricing from "./Pages/Host/HostVanPricing.jsx";
+import HostVanPhotos from "./Pages/Host/HostVanPhotos.jsx";
+
+import "../src/server.js";
 
 function App() {
 
   return (
-    <h1>Start here</h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="vans" element={<Vans />} />
+        <Route path="vans/:id" element={<VanDetail />} />
+        <Route path="login" element={<Login />} />
+
+        <Route element={<AuthRequired />}>
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+            <Route path="vans" element={<HostVans />} />
+            <Route path="vans/:id" element={<HostVanDetail />}>
+              <Route index element={<HostVanInfo />} />
+              <Route path="pricing" element={<HostVanPricing />} />
+              <Route path="photos" element={<HostVanPhotos />} />
+            </Route>
+          </Route>
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter, Routes } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from "./Pages/Home.jsx"
+import About from "./Pages/About.jsx"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -10,10 +29,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>,
 );
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-   <Routes>
-      
-      </Routes>
-  </BrowserRouter>
-);
